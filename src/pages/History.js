f15fdbffd2ce1ee272ba3d974562eecc1bb10da3
sheilaTing <835113231@qq.com>
2020-06-21 13:43:30 +0800

    code

---
+++ src/pages/History.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import List from '../components/List';
+function History(){
+    return(
+        <>
+            <h1>History</h1>
+            <List></List>
+        </>
+    )
+}
+
+export default History;
